1434f5a173df1617b65ce4f3612c150c7e0ed97c
松岡弘貴 <hiro@hirosMac-mini.local>
2024-12-10 21:02:25 +0900

    make search page UI

--- lib/screen/search_page.dart
+++ lib/screen/search_page.dart
@@ -12,6 +12,10 @@ class SearchPage extends StatefulWidget {
 class _SearchPageState extends State<SearchPage> {
   @override
   Widget build(BuildContext context) {
+
+    final TextEditingController searchController = TextEditingController();
+    final screenSize = MediaQuery.of(context).size;
+
     return Scaffold(
       appBar: AppBar(
         title: Text('検索', style: GoogleFonts.mochiyPopOne(
@@ -22,6 +26,128 @@ class _SearchPageState extends State<SearchPage> {
         iconTheme: const IconThemeData(
             color: Colors.white
         ),
+      ),
+
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: searchController,
+              decoration: const InputDecoration(
+                prefixIcon: Icon(Icons.search), // アイコンを設定
+                hintText: '近くのスポット', // ヒントテキストを表示
+                border: OutlineInputBorder(), // 外枠を追加
+              ),
+            ),
+          ),
+
+          Container(
+            width: screenSize.width*0.95,
+            height: screenSize.height*0.15,
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.black),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: screenSize.width*0.3,
+                  height: screenSize.height*0.12,
+                  color: Colors.grey,
+                  child: const Center(child: Text('Image')),
+                ),
+                
+                Container(
+                  width: screenSize.width*0.6,
+                  height: screenSize.height*0.12,
+                  child: const Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('場所の名前'),
+                      Text('営業時間'),
+                      Text('歯ブラシの残数'),
+                      Text('現在地からの距離'),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          const SizedBox(height: 30,),
+
+          Container(
+            width: screenSize.width*0.95,
+            height: screenSize.height*0.15,
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.black),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: screenSize.width*0.3,
+                  height: screenSize.height*0.12,
+                  color: Colors.grey,
+                  child: const Center(child: Text('Image')),
+                ),
+
+                Container(
+                  width: screenSize.width*0.6,
+                  height: screenSize.height*0.12,
+                  child: const Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('場所の名前'),
+                      Text('営業時間'),
+                      Text('歯ブラシの残数'),
+                      Text('現在地からの距離'),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          const SizedBox(height: 30,),
+
+          Container(
+            width: screenSize.width*0.95,
+            height: screenSize.height*0.15,
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.black),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: screenSize.width*0.3,
+                  height: screenSize.height*0.12,
+                  color: Colors.grey,
+                  child: const Center(child: Text('Image')),
+                ),
+
+                Container(
+                  width: screenSize.width*0.6,
+                  height: screenSize.height*0.12,
+                  child: const Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('場所の名前'),
+                      Text('営業時間'),
+                      Text('歯ブラシの残数'),
+                      Text('現在地からの距離'),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
